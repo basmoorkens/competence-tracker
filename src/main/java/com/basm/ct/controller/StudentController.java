9d40ff2ba37370b2e62a99682842938f9961d38a
basmoorkens <bas_moorkens@hotmail.com>
2018-08-22 23:24:07 +0200

    added find students by class

--- src/main/java/com/basm/ct/controller/StudentController.java
+++ src/main/java/com/basm/ct/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.basm.ct.controller;
 
 import com.basm.ct.model.EvaluationResult;
+import com.basm.ct.model.SchoolClass;
 import com.basm.ct.model.Student;
 import com.basm.ct.model.rest.StudentEvaluationResult;
 import com.basm.ct.model.rest.StudentViewModel;
+import com.basm.ct.repository.SchoolClassRepository;
 import com.basm.ct.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +19,26 @@ public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
 
+    @Autowired
+    private SchoolClassRepository schoolClassRepository;
+
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @RequestMapping(value = "/students", method =  RequestMethod.GET, produces = "application/json")
     public List<StudentViewModel> getStudents (){
         List<StudentViewModel> students = new ArrayList<>();
         for (Student student : studentRepository.findAll()) {
+            StudentViewModel studentViewModel = new StudentViewModel(student.getFirstName(), student.getName(), student.getSchoolClass().getName(), new ArrayList<>());
+            students.add(studentViewModel);
+        }
+        return students;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @RequestMapping(value = "/students/schoolclass/{id}", method =  RequestMethod.GET, produces = "application/json")
+    public List<StudentViewModel> getStudentsBySchoolClassId (@PathVariable("id") final Long schoolClassId){
+        SchoolClass foundClass = schoolClassRepository.findById(schoolClassId).get();
+        List<StudentViewModel> students = new ArrayList<>();
+        for (Student student : foundClass.getStudents()) {
             StudentViewModel studentViewModel = new StudentViewModel(student.getFirstName(), student.getName(), student.getSchoolClass().getName(), new ArrayList<>());
             students.add(studentViewModel);
         }
